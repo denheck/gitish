5372518ce7d2fa69623c75ce6add3bc80649e4cc
Dennis Heckman <denheck@gmail.com>
2019-03-16 23:10:36 -0500

    initial git cat-file

--- src/main/scala/gitish/main.scala
+++ src/main/scala/gitish/main.scala
@@ -2,19 +2,25 @@ package gitish
 
 import java.io.File
 import java.io.PrintWriter
-import java.nio.file.Paths
+import java.nio.file.{Paths, Path, Files}
+import java.util.zip.Inflater
 
 object Main {
   def main(args: Array[String]): Unit = {
-    args.length match {
-      case 1 => {
+    println(findGitDir("/Users/dennis/x/gitish"))
+    println(catFile("commit", "01d53d885c247ece575812a3cea08d9503c5a2ec"))
+  }
+
+  private def execCommand(args: Array[String]): Unit = {
+    if (args.length > 0) {
         args(0) match {
           case "init" => createRepo(System.getProperty("user.dir"))
+          case "cat-file" => catFile(args(1), args(2))
           case _ => println("gittish usage")
         }
+      } else {
+        println("gittish usage")
       }
-      case _ => println("gittish usage")
-    }
   }
 
   private def createRepo(path: String): Unit = {
@@ -31,5 +37,44 @@ object Main {
     val writer = new PrintWriter(Paths.get(path, ".git/HEAD").toFile)
 
     writer.write("ref: refs/heads/master\n")
+  }
+
+  private def catFile(_type: String, objectHash: String): String = {
+    val cwd = System.getProperty("user.dir")
+
+    findGitDir(cwd) match {
+      case Some(gitDir) => {
+        val (prefix, remainingHash) = objectHash.splitAt(2)
+        val objectFile = Paths.get(gitDir, "objects", prefix, remainingHash)
+        val objectBytes: Array[Byte] = Files.readAllBytes(objectFile)
+        val inflater = new Inflater
+
+        inflater.setInput(objectBytes)
+
+        val decompressedData = new Array[Byte](objectBytes.size * 2)
+        var count = inflater.inflate(decompressedData)
+        var finalData = decompressedData.take(count)
+        while (count > 0) {
+          count = inflater.inflate(decompressedData)
+          finalData = finalData ++ decompressedData.take(count)
+        }
+
+        new String(finalData.map(_.toChar))
+      }
+      case None => throw new Exception("Unable to find .git in " + cwd)
+    }
+  }
+
+  // search provided directory and parent directories for ".git"
+  private def findGitDir(directory: String): Option[String] = {
+    val p: Path = Paths.get(directory)
+
+    if (p == p.getRoot) {
+      None
+    } else if (Files.isDirectory(Paths.get(directory, ".git"))) {
+      Some(Paths.get(directory, ".git").toString)
+    } else {
+      findGitDir(Paths.get(directory).getParent.toString)
+    }
   }
 }
